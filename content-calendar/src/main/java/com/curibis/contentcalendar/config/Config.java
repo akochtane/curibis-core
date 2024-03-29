84e67eb2d5a9448d9c728719b8877c3039443a82
akochtane <50124918+akochtane@users.noreply.github.com>
2024-03-29 01:12:01 -0400

    Update Config.java

--- content-calendar/src/main/java/com/curibis/contentcalendar/config/Config.java
+++ content-calendar/src/main/java/com/curibis/contentcalendar/config/Config.java
@@ -1,7 +1,7 @@
 package com.curibis.contentcalendar.config;
 
 public class Config {
-    public static final String projectId = "vertex-ai-project-1-418219";
+    public static final String projectId = "your-project-id";
     public static final String location = "us-central1";
     public static final String modelName = "gemini-1.0-pro";
 }
